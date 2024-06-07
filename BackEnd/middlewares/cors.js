f08b4eab965aa50ad0661bca03034386de3833d1
jose Pertuz <japertuzd@gmail.com>
2024-06-07 15:38:40 -0500

    "Update accepted origin in CORS middleware from 'https://edukids-1.vercel.app/*' to 'https://edukids-1.vercel.app'"

--- BackEnd/middlewares/cors.js
+++ BackEnd/middlewares/cors.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 export const corsMiddleware=()=>cors({
     origin: (origin, callback) => {
       const ACCEPTED_ORIGINS = [
-        'https://edukids-1.vercel.app/*',
+        'https://edukids-1.vercel.app',
         'http://localhost:5173'
       ];
       if (ACCEPTED_ORIGINS.includes(origin)) {
