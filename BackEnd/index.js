2c3c31f271791997e01a9fed175744390c60c89e
jose Pertuz <japertuzd@gmail.com>
2024-06-07 16:10:22 -0500

    "Added import of fileURLToPath and updated __filename and __dirname variables"

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -4,6 +4,10 @@ import { routersProfesor } from './routes/routesProfesor.js';
 import { routersStudent } from './routes/routesStudent.js';
 import { corsMiddleware } from './middlewares/cors.js';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
