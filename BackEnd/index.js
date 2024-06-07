ee85853cc21c4ba884942084a8c0cc2c0c77a63a
jose Pertuz <japertuzd@gmail.com>
2024-06-07 15:00:30 -0500

    "Removed unnecessary comment, added express.urlencoded middleware, and reformatted code"

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -5,23 +5,21 @@ import { routersProfesor } from './routes/routesProfesor.js';
 import { routersStudent } from './routes/routesStudent.js';
 import { corsMiddleware } from './middlewares/cors.js';
 
-// Obtener __dirname en módulos ES
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
-const frontend = path.resolve(__dirname, '../../FrontEnd/dist'); // Ruta al directorio 'dist'
+const frontend = path.resolve(__dirname, '../../FrontEnd/dist');
 
 app.disable('x-powered-by');
 app.use(json());
 app.use(corsMiddleware());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/profesor', routersProfesor);
 app.use('/estudiante', routersStudent);
 
 app.use(express.static(frontend));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(frontend, 'index.html')); // Asegúrate de que el archivo index.html esté en 'dist'
+  res.sendFile(path.join(frontend, 'index.html'));
 });
 
 const port = process.env.PORT ?? 1234;
