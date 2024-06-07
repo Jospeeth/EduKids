fd03f2250039ac030998d3ca2c7a5ab7f1de2947
jose Pertuz <japertuzd@gmail.com>
2024-06-07 15:33:24 -0500

    fix bugs

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,12 +1,10 @@
 import express, { json } from 'express';
-import path from 'path';
 import { routersProfesor } from './routes/routesProfesor.js';
 import { routersStudent } from './routes/routesStudent.js';
 import { corsMiddleware } from './middlewares/cors.js';
 
 
 const app = express();
-const frontend = path.resolve(__dirname, '../../FrontEnd/dist');
 
 app.disable('x-powered-by');
 app.use(json());
@@ -17,9 +15,7 @@ app.use('/profesor', routersProfesor);
 app.use('/estudiante', routersStudent);
 
 app.use(express.static(frontend));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(frontend, 'index.html'));
-});
+
 
 const port = process.env.PORT ?? 1234;
 
