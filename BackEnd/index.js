1814898ea853265c0e0746b9e4a0b76de520a24d
jose Pertuz <japertuzd@gmail.com>
2024-06-07 16:00:27 -0500

    "Updated index.js: removed unnecessary import, added path import, replaced express.urlencoded with urlencoded, and added static file serving and catch-all route for React app"

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,19 +1,27 @@
-import express, { json } from 'express';
+import express from 'express';
+import { json, urlencoded } from 'express';
 import { routersProfesor } from './routes/routesProfesor.js';
 import { routersStudent } from './routes/routesStudent.js';
 import { corsMiddleware } from './middlewares/cors.js';
-
+import path from 'path';
 
 const app = express();
 
 app.disable('x-powered-by');
 app.use(json());
 app.use(corsMiddleware());
-app.use(express.urlencoded({ extended: true }));
+app.use(urlencoded({ extended: true }));
 
 app.use('/profesor', routersProfesor);
 app.use('/estudiante', routersStudent);
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Handles any requests that don't match the ones above
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 const port = process.env.PORT ?? 1234;
 
